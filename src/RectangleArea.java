7bdf836e76ddfee460e318f2f7d6058f1da8588b
brianwilkinson <brian.wilkinson@ssfs.org>
2020-06-04 14:18:35 -0400

    Initial commit

---
+++ src/RectangleArea.java
@@ -0,0 +1,7 @@
+public class RectangleArea {
+
+    public static int getArea(int length, int width) {
+
+        return length * width;
+    }
+}
---
+++ src/RectangleAreaTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RectangleAreaTest {
+
+    @Test
+    public void getArea() {
+
+        assertEquals(15, RectangleArea.getArea(3, 5));
+    }
+}
